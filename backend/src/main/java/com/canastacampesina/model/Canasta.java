2daaf88b3be51b694c335df0829669119584bcb1
Sebastian León <112210526+SekBack@users.noreply.github.com>
2022-09-27 10:15:54 -0500

    basic CRUD

--- backend/src/main/java/com/canastacampesina/model/Canasta.java
+++ backend/src/main/java/com/canastacampesina/model/Canasta.java
@@ -1,5 +1,38 @@
 package com.canastacampesina.model;
 
 public class Canasta {
-    
+    private int idProducto;
+    private String nombreProducto;
+    private int cantidadProducto;
+
+    public Canasta(){
+
+    }
+
+    public Canasta(int idProducto, String nombreProducto, int cantidadProducto){
+        this.idProducto=idProducto; 
+        this.nombreProducto=nombreProducto;
+        this.cantidadProducto=cantidadProducto;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public int getCantidadProducto() {
+        return cantidadProducto;
+    }
+
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public void setCantidadProducto(int cantidadProducto) {
+        this.cantidadProducto = cantidadProducto;
+    }
 }
